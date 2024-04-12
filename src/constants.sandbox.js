eedc2e102b64682040be65818ff5920975534cf0
Fabricker <joricklartigau@gmail.com>
2024-04-12 11:19:51 +0200

    AMOY migration

--- src/constants.sandbox.js
+++ src/constants.sandbox.js
@@ -1,7 +1,7 @@
 export const REACT_APP_MAGIC_PUBLISHABLE_KEY = "pk_live_79817E70A63BD8FB"
-export const ALCHEMY_NODE = "https://polygon-mumbai.g.alchemy.com/v2/bEVBAoWUjNsddFEVh1idpAzMwFX7tO1r"
+export const ALCHEMY_NODE = "https://polygon-amoy.g.alchemy.com/v2/FSnSzQYusAvaDzk-AiQQwzpfwFWFeo64"
 export const CHAIN_ID = 80001
-export const ERC20_ADDRESS = "0x5D7aA3749fb9bb9fe20534d26CB5a941d9e02871"
+export const ERC20_ADDRESS = "0xc3bD1a3769b32917602Ab88eB6F9cA4Cf84F8189"
 export const API_ROOT = "https://api.metabank.codinsight.app/api/v1"
 export const TOAST_OPTIONS = {
     position: "top-right",
@@ -12,4 +12,4 @@ export const TOAST_OPTIONS = {
     progress: undefined,
     theme: "colored",
 }
-export const EXPLORER = "https://mumbai.polygonscan.com"
+export const EXPLORER = "https://www.oklink.com/amoy"
