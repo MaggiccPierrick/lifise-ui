4ffba78361a09fc350c91d7a72394a278e165fc6
Fabricker <joricklartigau@gmail.com>
2024-05-10 16:21:16 +0200

    to prod

--- src/constants.prod.js
+++ src/constants.prod.js
@@ -1,8 +1,8 @@
 export const REACT_APP_MAGIC_PUBLISHABLE_KEY = "pk_live_30822EE505697402"
-export const ALCHEMY_NODE = "https://polygon-amoy.g.alchemy.com/v2/FSnSzQYusAvaDzk-AiQQwzpfwFWFeo64"
-export const CHAIN_ID = 80001
-export const ERC20_ADDRESS = "0xc3bD1a3769b32917602Ab88eB6F9cA4Cf84F8189"
-export const API_ROOT = "https://api.metabank.codinsight.app/api/v1"
+export const ALCHEMY_NODE = "https://polygon-mainnet.g.alchemy.com/v2/nFs45MKXjdrLEJ8WUdFkyQqC6ChQQHS7"
+export const CHAIN_ID = 137
+export const ERC20_ADDRESS = "0x820802Fa8a99901F52e39acD21177b0BE6EE2974"
+export const API_ROOT = "https://api.metabank-france.eu/api/v1"
 export const TOAST_OPTIONS = {
     position: "top-right",
     autoClose: 5000,
@@ -12,4 +12,4 @@ export const TOAST_OPTIONS = {
     progress: undefined,
     theme: "colored",
 }
-export const EXPLORER = "https://www.oklink.com/amoy"
+export const EXPLORER = "https://polygonscan.com"
